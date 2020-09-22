3efae00b393f3befd0f053afc1ccf8d61d035ab6
Javonte Tuppince <taetupp47@gmail.com>
2020-09-22 16:49:00 -0400

    made current time at the top of the page

---
+++ javascript.js
@@ -0,0 +1,16 @@
+// setting a varible to show current day 
+var day = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+// making a function for my current day
+$("#currentDay").text(day);
+
+//making a user inpu
+var toDo = "";
+
+
+
+    
+
+
+   
+    
